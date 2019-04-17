15807a19ebd2fcacf1d1ada712a6fb70acd37772
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-17 18:34:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,7 +133,7 @@ end
 end
 
 def shoe_size(name)
-  game_hash.each do |location, team_data|
+  game_hash().each do |location, team_data|
     team_data[:players].each do |player_name, player_stat|
       if name == player_name
        return player_stat[:shoe]
